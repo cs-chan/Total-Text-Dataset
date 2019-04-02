dc29c0a536a4f6ae2d0702d32eabea9e5a73dad2
ckchng <chngcheekheng@gmail.com>
2019-04-02 23:28:12 +1030

    Update Pascal_VOC.py

--- Evaluation_Protocol/Python_scripts/Pascal_VOC.py
+++ Evaluation_Protocol/Python_scripts/Pascal_VOC.py
@@ -49,6 +49,9 @@ def detection_filtering(detections, groundtruths, threshold=0.5):
 global_tp = 0
 global_fp = 0
 global_fn = 0
+global_num_of_gt = 0
+global_num_of_det = 0
+
 for input_id in allInputs:
     if (input_id != '.DS_Store'):
         print(input_id)
@@ -58,56 +61,60 @@ for input_id in allInputs:
         dc_id = np.where(groundtruths[:, 5] == '#')
         groundtruths = np.delete(groundtruths, (dc_id), (0))
 
-        iou_table = np.zeros((groundtruths.shape[0], len(detections)))
+        global_num_of_gt = global_num_of_gt + groundtruths.shape[0]
+        global_num_of_det = global_num_of_det + len(detections)
+        iou_table = np.zeros((len(detections), groundtruths.shape[0]))
         det_flag = np.zeros((len(detections), 1))
         gt_flag = np.zeros((groundtruths.shape[0], 1))
         tp = 0
         fp = 0
         fn = 0
-        for gt_id, gt in enumerate(groundtruths):
-            if len(detections) > 0:
-                for det_id, detection in enumerate(detections):
-                    detection = detection.split(',')
-                    detection = map(int, detection) #take it off in the final script
-                    det_y = detection[0::2]
-                    det_x = detection[1::2]
-                    gt_x = map(int, np.squeeze(gt[1]))
-                    gt_y = map(int, np.squeeze(gt[3]))
 
-                    iou_table[gt_id, det_id] = iou(det_x, det_y, gt_x, gt_y)
+        if len(detections) > 0:
+            for det_id, detection in enumerate(detections):
+                detection = detection.split(',')
+                detection = map(int, detection)
+                det_y = detection[0::2]
+                det_x = detection[1::2]
+                if len(groundtruths) > 0:
+                    for gt_id, gt in enumerate(groundtruths):
+                        gt_x = map(int, np.squeeze(gt[1]))
+                        gt_y = map(int, np.squeeze(gt[3]))
+                        iou_table[det_id, gt_id] = iou(det_x, det_y, gt_x, gt_y)
 
-                best_matched_det_id = np.argmax(iou_table[gt_id, :]) #identified the best matched detection candidates with current groundtruth
+                    best_matched_gt_id = np.argmax(iou_table[det_id, :]) #identified the best matched detection candidates with current groundtruth
+                    if (iou_table[det_id, best_matched_gt_id] >= 0.5):
+                        if gt_flag[best_matched_gt_id] == 0: ### if the gt is already matched previously, it should be a false positive
+                            tp = tp + 1.0
+                            global_tp = global_tp + 1.0
+                            gt_flag[best_matched_gt_id] = 1
+                        else:
+                            fp = fp + 1.0
+                            global_fp = global_fp + 1.0
+                    else:
+                        fp = fp + 1.0
+                        global_fp = global_fp + 1.0
 
-                matched_id = np.where(iou_table[gt_id, :] >= 0.5)
-                if (iou_table[gt_id, best_matched_det_id] >= 0.5):
-                    if matched_id[0].shape[0] < 2:
-                        tp = tp + 1.0
-                        global_tp = global_tp + 1.0
-                        det_flag[best_matched_det_id] = 1
-                        gt_flag[gt_id] = 1
+        try:
+            local_precision = tp / (tp + fp)
+        except ZeroDivisionError:
+            local_precision = 0
 
-        #Update local and global tp, fp, and fn
-        inv_gt_flag = 1 - gt_flag
-        fn = np.sum(inv_gt_flag)
-        inv_det_flag = 1 - det_flag
-        fp = fp + np.sum(inv_det_flag)
+        try:
+            local_recall = tp / groundtruths.shape[0]
+        except ZeroDivisionError:
+            local_recall = 0
 
-        global_fp = global_fp + fp
-        global_fn = global_fn + fn
-        local_precision = tp / (tp + fp)
-        local_recall = tp / (tp + fn)
         fid = open(fid_path, 'a')
         temp = ('%s______/Precision:_%s_______/Recall:_%s\n' %(input_id, str(local_precision), str(local_recall)))
         fid.write(temp)
         fid.close()
-
-
+            
 global_precision = global_tp / (global_tp + global_fp)
-global_recall = global_tp / (global_tp + global_fn)
+global_recall = global_tp / global_num_of_gt
 f_score = 2*global_precision*global_recall/(global_precision+global_recall)
 
 fid = open(fid_path, 'a')
 temp = ('Precision:_%s_______/Recall:_%s\n' %(str(global_precision), str(global_recall)))
 fid.write(temp)
 fid.close()
-print('pb')
